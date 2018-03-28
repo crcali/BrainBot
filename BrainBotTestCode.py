7166876e954ba06e5d98cd1cfe92219fd918f82a
crcali <33082459+crcali@users.noreply.github.com>
2018-03-27 23:37:45 -0700

    Refines wait time

--- BrainBotTestCode.py
+++ BrainBotTestCode.py
@@ -9,148 +9,153 @@ def moveForward(speed, distance):
 	#commands to move forward with the parameters
 	numberOfTimes = (distance/5)*2
 
+	waitTime = (speed/1000)+.25
 
 	for x in range (0,numberOfTimes):
 		#lifts up first leg 
 		sp.write(("#25 P1600 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 		sp.write(("#24 P1200 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 		sp.write(("#25 P2215 T%s\r" %speed).encode())
 		
-		time.sleep(.5)
+		time.sleep(waitTime)
 
 		#lifts up second leg 
 		sp.write(("#1 P1535 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 		sp.write(("#0 P1950 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 		sp.write(("#1 P2150 T%i\r" %speed).encode())
 
-		time.sleep(.5)
+		time.sleep(waitTime)
 
 		#lifts up third leg 
 		sp.write(("#17 P2500 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 		sp.write(("#16 P1000 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 		sp.write(("#17 P1600 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 
-		time.sleep(.5)
+		time.sleep(waitTime)
 
 		#lifts up forth leg 
 		sp.write(("#9 P1600 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 		sp.write(("#8 P1900 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 		sp.write(("#9 P2250 T%i\r" %speed).encode())
-		time.sleep(.5)
+		time.sleep(waitTime)
 
-		time.sleep(.5)
+		time.sleep(waitTime)
 
 		#moves body forward
 		sp.write(("#0 P1425 #8 P1500 #16 P1500 #24 P1600 #26 P1250 T400\r").encode())
 
-		time.sleep(2)
+		time.sleep(waitTime)
 
 #sets up the move backward module
 def moveBackward(speed, distance):
 	#commands to move backward with the parameters
 	numberOfTimes = distance
 
+	waitTime = (speed/1000)+.25
+
 	#!!run for x in range command when distance is refined!! 
 	#lifts up first leg 
 	sp.write(("#25 P1600 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#24 P2200 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#25 P2215 T%s\r" %speed).encode())
 	
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
 	#lifts up second leg 
 	sp.write(("#1 P1535 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#0 P1000 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#1 P2150 T%i\r" %speed).encode())
 
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
 	#lifts up third leg 
 	sp.write(("#17 P2500 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#16 P1900 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#17 P1600 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
 	#lifts up forth leg 
 	sp.write(("#9 P1600 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#8 P900 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#9 P2250 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
 	#moves body 
 	sp.write(("#0 P1425 #8 P1500 #16 P1500 #24 P1600 #26 P1250 T400\r").encode())
 
-	time.sleep(2)
+	time.sleep(waitTime)
 
 #sets up the move backward module
 def turn(speed, degrees):
 	#commands to move backward with the parameters
 	numberOfTimes = degrees
 
+	waitTime = (speed/1000)+.25
+
 	#!!run for x in range command when distance is refined!! 
 	#lifts up first leg 
 	sp.write(("#25 P1600 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#24 P2200 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#25 P2215 T%s\r" %speed).encode())
 	
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
 	#lifts up second leg 
 	sp.write(("#1 P1535 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#0 P1000 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#1 P2150 T%i\r" %speed).encode())
 
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
 	#lifts up third leg 
 	sp.write(("#17 P2500 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#16 P1900 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#17 P1600 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
 	#lifts up forth leg 
 	sp.write(("#9 P1600 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#8 P900 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 	sp.write(("#9 P2250 T%i\r" %speed).encode())
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
-	time.sleep(.5)
+	 time.sleep(waitTime)
 
 	#moves body 
 	sp.write(("#0 P1425 #8 P1500 #16 P1500 #24 P1600 #26 P1250 T400\r").encode())
 
-	time.sleep(2)
+	time.sleep(waitTime)
 
 #the defult, rest position 
 def defultPosition():

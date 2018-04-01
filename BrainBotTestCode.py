9ad678a74c0e5b786e258a93bc8624c585d609ea
crcali <33082459+crcali@users.noreply.github.com>
2018-04-01 15:29:57 -0700

    Cleans up code and refines backward movement

--- BrainBotTestCode.py
+++ BrainBotTestCode.py
@@ -4,6 +4,10 @@ import time
 #open a serial port
 sp = serial.Serial('/dev/ttyUSB0', 9600, timeout=0)
 
+def moveServo(servoNumber, position, speed, waitTime):
+	sp.write(("#%i P%i T%i\r" %(servoNumber, position, speed)).encode())
+	time.sleep(waitTime)
+
 #sets up the move forward module 
 def moveForward(speed, distance):
 	#commands to move forward with the parameters
@@ -13,44 +17,28 @@ def moveForward(speed, distance):
 
 	for x in range (0,numberOfTimes):
 		#lifts up first leg 
-		sp.write(("#25 P1600 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#24 P1200 T%i\r" %speed).encode())
-		time.sleep(waitTime)
+		moveServo(25, 1600, speed, waitTime)
+		moveServo(24, 1200, speed, waitTime)
 		sp.write(("#25 P2215 #26 P1450 T%i\r" %speed).encode())
-		
 		time.sleep(waitTime)
 
 		#lifts up second leg 
-		sp.write(("#1 P1535 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#0 P1950 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#1 P2150 T%i\r" %speed).encode())
-
-		time.sleep(waitTime)
+		moveServo(1, 1535, speed, waitTime)
+		moveServo(0, 1950, speed, waitTime)
+		moveServo(1, 2150, speed, waitTime)
 
 		#lifts up third leg 
-		sp.write(("#17 P2500 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#16 P1000 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#17 P1600 T%i\r" %speed).encode())
-
-		time.sleep(waitTime)
+		moveServo(17, 2500, speed, waitTime)
+		moveServo(16, 1000, speed, waitTime)
+		moveServo(17, 1600, speed, waitTime)
 
 		#lifts up forth leg 
-		sp.write(("#9 P1600 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#8 P1900 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#9 P2250 T%i\r" %speed).encode())
-
-		time.sleep(waitTime)
+		moveServo(9, 1600, speed, waitTime)
+		moveServo(8, 1900, speed, waitTime)
+		moveServo(9, 2250, speed, waitTime)
 
 		#moves body forward
 		sp.write(("#0 P1425 #8 P1500 #16 P1500 #24 P1600 #26 P1250 T400\r").encode())
-
 		time.sleep(waitTime)
 
 #sets up the turn module
@@ -62,48 +50,32 @@ def moveBackward(speed, distance):
 
 	#!!run for x in range command when distance is refined!! 
 	#lifts up first leg 
-	sp.write(("#25 P1600 T%i\r" %speed).encode())
-	time.sleep(waitTime)
-	sp.write(("#24 P2200 T%i\r" %speed).encode())
-	time.sleep(waitTime)
+	moveServo(25, 1600, speed, waitTime)
+	moveServo(24, 2200, speed, waitTime)
 	sp.write(("#25 P2215 #26 P1450 T%i\r" %speed).encode())
-	
 	time.sleep(waitTime)
 
 	#lifts up second leg 
-	sp.write(("#1 P1535 T%i\r" %speed).encode())
-	time.sleep(waitTime)
-	sp.write(("#0 P1950 T%i\r" %speed).encode())
-	time.sleep(waitTime)
-	sp.write(("#1 P2150 T%i\r" %speed).encode())
-
-	time.sleep(waitTime)
+	moveServo(1, 1535, speed, waitTime)
+	moveServo(0, 950, speed, waitTime)
+	moveServo(1, 2150, speed, waitTime)
 
 	#lifts up third leg 
-	sp.write(("#17 P2500 T%i\r" %speed).encode())
-	time.sleep(waitTime)
-	sp.write(("#16 P1900 T%i\r" %speed).encode())
-	time.sleep(waitTime)
-	sp.write(("#17 P1600 T%i\r" %speed).encode())
-
-	time.sleep(waitTime)
+	moveServo(17, 2250, speed, waitTime)
+	moveServo(16, 1900, speed, waitTime)
+	moveServo(17, 1600, speed, waitTime)
 
 	#lifts up forth leg 
-	sp.write(("#9 P1600 T%i\r" %speed).encode())
-	time.sleep(waitTime)
-	sp.write(("#8 P900 T%i\r" %speed).encode())
-	time.sleep(waitTime)
-	sp.write(("#9 P2250 T%i\r" %speed).encode())
-
-	time.sleep(waitTime)
+	moveServo(9, 1600, speed, waitTime)
+	moveServo(8, 900, speed, waitTime)
+	moveServo(9, 2250, speed, waitTime)
 
 	#moves body 
 	sp.write(("#0 P1425 #8 P1500 #16 P1500 #24 P1600 #26 P1250 T400\r").encode())
-
 	time.sleep(waitTime)
 
 #sets up the move backward module
-def turn(speed, degrees):
+def turnCounterClockwise(speed, degrees):
 	#commands to move backward with the parameters
 	numberOfTimes = degrees/45
 
@@ -111,89 +83,56 @@ def turn(speed, degrees):
 
 	for x in range (0,numberOfTimes):
 		#lifts up first leg 
-		sp.write(("#25 P1600 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#24 P2700 T%i\r" %speed).encode())
-		time.sleep(waitTime)
+		moveServo(25, 1600, speed, waitTime)
+		moveServo(24, 2700, speed, waitTime)
 		sp.write(("#25 P2215 #26 P1450 T%i\r" %speed).encode())
-		
 		time.sleep(waitTime)
 
 		#lifts up second leg 
-		sp.write(("#1 P1535 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#0 P2300 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#1 P2150 T%i\r" %speed).encode())
-
-		time.sleep(waitTime)
+		moveServo(1, 1535, speed, waitTime)
+		moveServo(0, 2300, speed, waitTime)
+		moveServo(1, 2150, speed, waitTime)
 
 		#lifts up third leg 
-		sp.write(("#17 P2500 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#16 P2300 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#17 P1600 T%i\r" %speed).encode())
-
-		time.sleep(waitTime)
+		moveServo(17, 2500, speed, waitTime)
+		moveServo(16, 2300, speed, waitTime)
+		moveServo(17, 1600, speed, waitTime)
 
 		#lifts up forth leg 
-		sp.write(("#9 P1600 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#8 P2350 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#9 P2250 T%i\r" %speed).encode())
-		time.sleep(waitTime)
+		moveServo(9, 1600, speed, waitTime)
+		moveServo(8, 2350, speed, waitTime)
+		moveServo(9, 2250, speed, waitTime)
 
 		time.sleep(waitTime)
 
 		#moves body forward
 		sp.write(("#0 P1425 #8 P1500 #16 P1500 #24 P1600 #26 P1250 T400\r").encode())
-
 		time.sleep(waitTime)
-
-		#fine tunes the turning for more accurate movement
 
 		#lifts up first leg 
-		sp.write(("#25 P1600 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#24 P1650 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#25 P2215 #26 P1450 T%i\r" %speed).encode())
-		
-		time.sleep(waitTime)
+		moveServo(25, 1600, speed, waitTime)
+		moveServo(24, 1650, speed, waitTime)
+		moveServo(25, 2215, speed, waitTime)
 
 		#lifts up second leg 
-		sp.write(("#1 P1535 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#0 P1475 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#1 P2150 T%i\r" %speed).encode())
-
-		time.sleep(waitTime)
+		moveServo(1, 1535, speed, waitTime)
+		moveServo(0, 1475, speed, waitTime)
+		moveServo(1, 2150, speed, waitTime)
 
 		#lifts up third leg 
-		sp.write(("#17 P2500 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#16 P1650 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#17 P1600 T%i\r" %speed).encode())
-
-		time.sleep(waitTime)
+		moveServo(17, 2500, speed, waitTime)
+		moveServo(16, 1650, speed, waitTime)
+		moveServo(17, 1600, speed, waitTime)
 
 		#lifts up forth leg 
-		sp.write(("#9 P1600 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#8 P1650 T%i\r" %speed).encode())
-		time.sleep(waitTime)
-		sp.write(("#9 P2250 T%i\r" %speed).encode())
-		time.sleep(waitTime)
+		moveServo(9, 1600, speed, waitTime)
+		moveServo(8, 1650, speed, waitTime)
+		moveServo(9, 2250, speed, waitTime)
 
 		time.sleep(waitTime)
 
 		#moves body forward
 		sp.write(("#0 P1425 #8 P1500 #16 P1500 #24 P1600 #26 P1250 T400\r").encode())
-
 		time.sleep(waitTime)
 
 #the defult, rest position 
